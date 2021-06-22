1f37e665248bed3e3d780895dd27ca1b86cdfb1b
Charles Frank <charles@luckyfrankstudios.com>
2021-06-21 19:44:23 -0700

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
 function App() {
+
+  const [volume, setVolume] = React.useState(1);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className = "bg-info text-white" id = "drum-machine">
+<div id = "display" className  = "text-center">
+  <h1>Drum Machine</h1>
+{bankOne.map((clip) => (
+  <Pad clip = {clip} key = {clip.id} volume={volume} />
+)
+)}
+<br />
+<h4>Volume</h4>
+<input type = "range" step = "0.01" max="1" 
+min = "0" value = {volume} className ="w-50" onChange = {(e) => setVolume(e.target.value)}/>
+
+</div>
+
+</div>
   );
+}
+
+
+function Pad({clip, volume}){
+
+const [instrument, setInstrument] = React.useState("");
+const [active, setActive] = React.useState(false);
+
+React.useEffect(() =>{
+document.addEventListener("keydown", handleKeyPress);
+return () => {
+  document.removeEventListener("keydown", handleKeyPress);
+};
+}, []);
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === clip.keyCode) {
+      playSound()
+    }
+  }
+
+
+
+  const playSound = () => {
+    
+    const audioTag = document.getElementById(clip.keyTrigger)
+  setActive(true);
+    setTimeout(() => setActive(false, 200))
+    setInstrument(() => clip.id)
+    audioTag.volume = volume;
+    audioTag.currentTime = 0
+    audioTag.play()
+    
+  }
+
+  return (
+    <div className = {`btn btn-primary ${active && "btn-warning"} drum-pad`} onClick = {playSound}>
+      <audio className = "clip" id = {clip.keyTrigger} src = {clip.url} />
+      {clip.keyTrigger}
+    </div>
+  )
 }
 
 export default App;
